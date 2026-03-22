674d5ff41f42fe12b57f83835e01121ef31f8bcb
orhan <orhan.bedir@gmail.com>
2026-03-22 14:51:09 +0300

    test no db

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const pool = require("./db");
+//const pool = require("./db");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+
+app.get("/", (req, res) => {
+  res.send("API çalışıyor 🚀");
+});
 
 // =========================
 // JWT TOKEN ÜRET
