ca7b2526d0c41a146d157026623d294477fd7f00
orhan <orhan.bedir@gmail.com>
2026-03-23 09:28:27 +0300

    connect to railway backend

--- backend/server.js
+++ backend/server.js
@@ -435,16 +435,7 @@ app.post("/api/avanslar", async (req, res) => {
   }
 });
 
-// deneme silinecek=========================
-app.get("/reset-avanslar", async (req, res) => {
-  try {
-    await pool.query("TRUNCATE TABLE avanslar RESTART IDENTITY;");
-    res.send("Avans tablosu sifirlandi");
-  } catch (err) {
-    console.error("RESET ERROR:", err);
-    res.status(500).send(err.message);
-  }
-});
+
 
 
 // AVANS LİSTE
